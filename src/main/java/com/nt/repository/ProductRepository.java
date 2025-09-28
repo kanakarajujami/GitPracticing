32a6e15769c389ce1454e2e277c836da8ef74639
kanakarajujami <kanakarajujami409@gmail.com>
2025-09-28 16:41:01 +0530

    Revert "custom finder methods added in respository"
    This reverts commit 5630e10e2ba6b569e276abef5e8de7fe2567a63b.

--- src/main/java/com/nt/repository/ProductRepository.java
+++ src/main/java/com/nt/repository/ProductRepository.java
@@ -3,14 +3,6 @@ package com.nt.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.nt.entity.Product;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // Additional query methods (if needed) can be defined here
-
-
-    public Optional<Product> findProductByName(String name);
-
-    public List<Product>  fetchAllProductsByPriceRange(Double start,Double end);
 }
